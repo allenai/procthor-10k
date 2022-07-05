c3a0da6057d088e92ee518e04b50f6565fdc1c34
lucaw <lucaw@allenai.org>
2022-07-05 11:19:36 -0700

    Optional parameters to control length of returned dataset.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import gzip
 import json
+from typing import Optional, Dict, Any
 
 import prior
 from tqdm import tqdm
+
 
 # import pickle
 
@@ -34,15 +36,29 @@ from tqdm import tqdm
 #     f.write(json.dumps(houses["test"]).encode("utf-8"))
 
 
-def load_dataset() -> prior.DatasetDict:
+def load_dataset(config: Optional[Dict[str, Any]] = None) -> prior.DatasetDict:
     """Load the houses dataset."""
+
+    if config is None:
+        config = {}
+
+    train_size = config.get("train_size", 10_000)
+    val_size = config.get("val_size", 1_000)
+    test_size = config.get("test_size", 1_000)
+
+    assert 1 <= len(train_size) <= int(1e-4)
+    assert 1 <= len(val_size) <= int(1e-3)
+    assert 1 <= len(test_size) <= int(1e-3)
+
     data = {}
-    for split, size in [("train", 10_000), ("val", 1_000), ("test", 1_000)]:
+    for split, size in [("train", train_size), ("val", val_size), ("test", test_size)]:
         with gzip.open(f"{split}.jsonl.gz", "r") as f:
-            houses = [
-                json.loads(line)
-                for line in tqdm(f, total=size, desc=f"Loading {split}")
-            ]
+            houses = []
+            for line in tqdm(f, total=size, desc=f"Loading {split}"):
+                if len(houses) == size:
+                    break
+                houses.append(json.loads(line))
+
         data[split] = prior.Dataset(
             data=houses, dataset="procthor-dataset", split=split
         )
