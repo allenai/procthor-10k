439193522244720b86d8c81cde2e51e3a4d150cf
mattdeitke <28768645+mattdeitke@users.noreply.github.com>
2022-12-13 16:55:54 -0800

    update procthor-10k

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import gzip
 
-from tqdm import tqdm
-
 import prior
+from tqdm import tqdm
 
 try:
     from prior import LazyJsonDataset
@@ -12,6 +11,12 @@ except:
 
 def load_dataset() -> prior.DatasetDict:
     """Load the houses dataset."""
+    print(
+        "[AI2-THOR WARNING] There has been an update to ProcTHOR-10K that must be used with AI2-THOR version 5.0+. To use the new version of ProcTHOR-10K, please update AI2-THOR to version 5.0+ by running:\n"
+        "    pip install --upgrade ai2thor\n"
+        "Alternatively, to downgrade to the old version of ProcTHOR-10K, run:\n"
+        '   prior.load_dataset("procthor-10k", revision="ab3cacd0fc17754d4c080a3fd50b18395fae8647")'
+    )
     data = {}
     for split, size in [("train", 10_000), ("val", 1_000), ("test", 1_000)]:
         with gzip.open(f"{split}.jsonl.gz", "r") as f:
